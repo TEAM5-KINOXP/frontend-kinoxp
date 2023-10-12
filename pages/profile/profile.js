96dd5ccd7f2c373db7476f991f52449e2663fac2
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2023-10-12 11:32:08 +0200

    T5K-75-security-settings frontend profile

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,20 +1,20 @@
 import { handleHttpErrors, makeOptions, sanitizeStringWithTableRows } from "../../utility.js";
 import { API_URL } from "../../settings.js";
-const HARDCODEDUSER = API_URL + "/users/username1"
+const URL = API_URL + "/users"
 
 export async function initProfile(){
     document.querySelector('#email').value = ""
     document.querySelector('#first-name').value = ""
     document.querySelector('#last-name').value = ""
 
-    const profileInfo = await fetch(HARDCODEDUSER).then(handleHttpErrors)
+    const profileInfo = await fetch(URL + "/users-for-authenticated", makeOptions("GET", null, true)).then(handleHttpErrors)
     if(profileInfo){
         document.querySelector('#title').innerHTML = "Type new information for profile user: "+profileInfo.username
         document.querySelector('#email').value = profileInfo.email
         document.querySelector('#first-name').value = profileInfo.firstName
         document.querySelector('#last-name').value = profileInfo.lastName
     }
-    const reservations = await fetch(API_URL+"/reservations/username1").then(handleHttpErrors)
+    const reservations = await fetch(API_URL+"/reservations/reservations-for-authenticated", makeOptions("GET", null, true)).then(handleHttpErrors)
     const reservationRows = reservations.map(res =>`
         <tr>
             <td>${res.movieTitle}</td>
@@ -34,18 +34,17 @@ async function setUpEditModal(email, firstName, lastName){
     document.querySelector('#last-name-input').value = lastName
 }
 async function editUser(){
-    const savedUsername = "username1"
     const newPassword = document.querySelector('#password-input').value
     const newEmail = document.querySelector('#email-input').value
     const newFirstName = document.querySelector('#first-name-input').value
     const newLastName =document.querySelector('#last-name-input').value
     const updatedUser = {
-        username : savedUsername,
+        username : localStorage.getItem("user"),
         password : newPassword,
         email : newEmail,
         firstName : newFirstName,
         lastName : newLastName
     }
-    await fetch(HARDCODEDUSER, makeOptions("PUT", updatedUser)).then(handleHttpErrors)
+    await fetch(URL + "/users-for-authenticated", makeOptions("PUT", updatedUser,true)).then(handleHttpErrors)
     location.reload()
 }
