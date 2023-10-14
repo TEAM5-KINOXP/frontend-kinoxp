2c17dc4978abb8161708370d015b537c0cb62b27
Mathias Kaae <117026503+MKaae@users.noreply.github.com>
2023-10-14 22:19:41 +0200

    Update settings.js
    changes API url to static web app

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 
-//export const API_URL = "https://kinoxpbackendg5.azurewebsites.net/api"
-export const API_URL = "http://localhost:8080/api"
+export const API_URL = "https://kinoxpbackendg5.azurewebsites.net/api"
+//export const API_URL = "http://localhost:8080/api"
 
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
